31dd4652d1b52365984a1fa7eb7eb54a9f390dfb
Andriana_Yarmoliuk <gitHub2401>
2016-11-15 18:58:53 +0200

    added script for test data

--- src/main/resources/CREATE_TABLE_Users.sql
+++ src/main/resources/CREATE_TABLE_Users.sql
@@ -1,10 +1,15 @@
 CREATE TABLE Users (
   id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
   firstName VARCHAR (50),
-  nickName VARCHAR (50),
   lastName VARCHAR (50),
   telephone VARCHAR (25),
   email VARCHAR (50),
-  memberSince DATE DEFAULT '0000-00-00',
   PRIMARY KEY (id)
 );
+
+insert into users (firstName, lastName, telephone, email) VALUES ('Andriana', 'yarmoliuk', '95476345', 'ersgr5674eswg');
+insert into users (firstName, lastName, telephone, email) VALUES ('Andriftgeana', 'yarmor4wh5rtliuk', '954763rtgh45', 'ersgr447eswg');
+insert into users (firstName, lastName, telephone, email) VALUES ('Anddrebtredtriana', 'yartrbhmoliuk', '9547rtbh6345', 'ers45645gr4eswg');
+insert into users (firstName, lastName, telephone, email) VALUES ('Anrbtvgb driana', 'yarmrtfbholiuk', '9547646y75345', 'ers4675gr4eswg');
+insert into users (firstName, lastName, telephone, email) VALUES ('Andrbr tgb iana', 'yarmrhrtoliuk', '9547567566345', 'ersg457r4eswg');
+insert into users (firstName, lastName, telephone, email) VALUES ('Andrbnrriana', 'yarmolrtgsbhriuk', '95456776345', 'ersg457r4eswg');
