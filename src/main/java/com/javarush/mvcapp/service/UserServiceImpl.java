43e1fb8e8f7b22eb5214c7f2893c942d6b6ebf1f
Andriana_Yarmoliuk <gitHub2401>
2016-11-15 12:51:19 +0200

    added missing service annotation

--- src/main/java/com/javarush/mvcapp/service/UserServiceImpl.java
+++ src/main/java/com/javarush/mvcapp/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.javarush.mvcapp.service;
 import com.javarush.mvcapp.dao.UserDao;
 import com.javarush.mvcapp.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -10,6 +11,8 @@ import java.util.List;
 /**
  * Created by Andriana on 14.11.2016.
  */
+
+@Service
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserDao userDao;
