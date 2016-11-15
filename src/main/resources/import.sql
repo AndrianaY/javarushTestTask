70c58475d73111d357cfb03090dac6e086ddcc60
Andriana_Yarmoliuk <gitHub2401>
2016-11-15 20:01:06 +0200

    added auto script for prefilling test data

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+insert into users (firstName, lastName, telephone, email) VALUES ('Andriana', 'yarmoliuk', '95476345', 'ersgr5674eswg');
+insert into users (firstName, lastName, telephone, email) VALUES ('Andriftgeana', 'yarmor4wh5rtliuk', '954763rtgh45', 'ersgr447eswg');
+insert into users (firstName, lastName, telephone, email) VALUES ('Anddrebtredtriana', 'yartrbhmoliuk', '9547rtbh6345', 'ers45645gr4eswg');
+insert into users (firstName, lastName, telephone, email) VALUES ('Anrbtvgb driana', 'yarmrtfbholiuk', '9547646y75345', 'ers4675gr4eswg');
+insert into users (firstName, lastName, telephone, email) VALUES ('Andrbr tgb iana', 'yarmrhrtoliuk', '9547567566345', 'ersg457r4eswg');
+insert into users (firstName, lastName, telephone, email) VALUES ('Andrbnrriana', 'yarmolrtgsbhriuk', '95456776345', 'ersg457r4eswg');
